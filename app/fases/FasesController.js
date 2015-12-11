3941091dbe3fe7d0f0b1efc459212f6c6fb3d7f2
Leonardo <leonardo.giroto@gmail.com>
2015-12-11 21:29:36 -0200

    Primeiro Controller na Estrutura

---
+++ app/fases/FasesController.js
@@ -0,0 +1,37 @@
+/**
+ * Main application controller
+ *
+ * You can use this controller for your whole app if it is small
+ * or you can have separate controllers for each logical section
+ * 
+ */
+;(function() {
+
+  angular
+    .module('boilerplate')
+    .controller('FasesController', FasesController);
+
+  FasesController.$inject = ['LocalStorage', 'QueryService'];
+
+
+  function FasesController(LocalStorage, QueryService) {
+
+    // 'controller as' syntax
+    var self = this;
+
+
+    ////////////  function definitions
+
+
+    /**
+     * Load some data
+     * @return {Object} Returned object
+     */
+    // QueryService.query('GET', 'posts', {}, {})
+    //   .then(function(ovocie) {
+    //     self.ovocie = ovocie.data;
+    //   });
+  }
+
+
+})();
