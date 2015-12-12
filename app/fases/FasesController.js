4e0cd03ab4a4ef26404ba953d850aef88b69d1db
Leonardo <leonardo.giroto@gmail.com>
2015-12-12 01:17:29 -0200

    Ajustes bundle e FasesController

--- app/fases/FasesController.js
+++ app/fases/FasesController.js
@@ -1,10 +1,4 @@
-/**
- * Main application controller
- *
- * You can use this controller for your whole app if it is small
- * or you can have separate controllers for each logical section
- * 
- */
+
 
 ;(function() {
 
@@ -24,32 +18,9 @@
     // 'controller as' syntax
     var vm = this;
 
-    console.log('teste');
-
-    $scope.teste = "teste";
-
     var ref = new Firebase(FIREBASE_URI);
 
-    var userRef = ref.child('users');
-    var pintoRef = ref.child('pintos');
-
     var faseRef = ref.child('fases');
-
-    userRef.set({
-              amanda: {
-                friends: { "brinchen": true },
-                name: "Mary Chen",
-                widgets: { "one": true, "three": true }
-              }
-          });
-
-    pintoRef.set({
-              pintoso: {
-                friends: { "brinchen": true },
-                name: "Mary Chen",
-                widgets: { "one": true, "three": true }
-              }
-          });
 
     faseRef.set({
           pessoa: {
@@ -60,14 +31,6 @@
           }
     });
 
-    faseRef.set({
-          Hue: {
-            name: "Leonardo",
-            type: TypeEnum.POLITICIAN,
-            age: 12,
-            profession: "Engenheiro"
-          }
-    });
   }
 
   
