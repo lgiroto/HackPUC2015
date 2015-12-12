b823981bc6f07de9b8bcea8ec8fceb00dfac7e02
JoaoJardim <jgtjardim@gmail.com>
2015-12-11 22:21:01 -0200

    Firebase no controller

--- app/fases/FasesController.js
+++ app/fases/FasesController.js
@@ -5,6 +5,10 @@
  * or you can have separate controllers for each logical section
  * 
  */
+
+ var Firebase = require("firebase");
+
+
 ;(function() {
 
   angular
@@ -17,20 +21,23 @@
   function FasesController(LocalStorage, QueryService) {
 
     // 'controller as' syntax
-    var self = this;
+    var vm = this;
+
+    var ref = new Firebase('https://boiling-inferno-5866.firebaseio.com/');
+
+    ref.set({
+      alanisawesome: {
+        date_of_birth: "June 2345258, 1912",
+        full_name: "Alan Turing"
+      },
+      gracehop: {
+        date_of_birth: "December 9, 1906",
+        full_name: "Grace Hopper"
+      }
+    });
+    
 
 
-    ////////////  function definitions
-
-
-    /**
-     * Load some data
-     * @return {Object} Returned object
-     */
-    // QueryService.query('GET', 'posts', {}, {})
-    //   .then(function(ovocie) {
-    //     self.ovocie = ovocie.data;
-    //   });
   }
 
 
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,2 +1,23 @@
 var Firebase = require("firebase");
 
+
+var ref = new Firebase('https://boiling-inferno-5866.firebaseio.com/');
+
+ref.set({
+  alanisawesome: {
+    date_of_birth: "June 23, 1912",
+    full_name: "Alan Turing"
+  },
+  gracehop: {
+    date_of_birth: "December 9, 1906",
+    full_name: "Grace Hopper"
+  }
+});
+
+
+
+
+
+
+
+
