cb01b12b8ef07b57c0e19f154cde6a5e9ac22afd
Leonardo <leonardo.giroto@gmail.com>
2015-12-11 23:21:39 -0200

    Primeiro controller funcionando

--- firebase/bundle.js
+++ firebase/bundle.js
@@ -46,19 +46,7 @@
 
 	var Firebase = __webpack_require__(1);
 
-
 	var ref = new Firebase('https://boiling-inferno-5866.firebaseio.com/');
-
-	ref.set({
-	  alanisawesome: {
-	    date_of_birth: "June 23, 1912",
-	    full_name: "Alan Turing"
-	  },
-	  gracehop: {
-	    date_of_birth: "December 9, 1906",
-	    full_name: "Grace Hopper"
-	  }
-	});
 
 
 
--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,19 +1,6 @@
 var Firebase = require("firebase");
 
-
 var ref = new Firebase('https://boiling-inferno-5866.firebaseio.com/');
-
-ref.set({
-  alanisawesome: {
-    date_of_birth: "June 23, 1912",
-    full_name: "Alan Turing"
-  },
-  gracehop: {
-    date_of_birth: "December 9, 1906",
-    full_name: "Grace Hopper"
-  }
-});
-
 
 
 
